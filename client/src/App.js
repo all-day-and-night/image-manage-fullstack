c4f0d167607a7beae699bf1bf8474717356cc05c
index <jwug0821@gmail.com>
2022-09-28 17:18:14 +0900

    apply useContext for images

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
+import ImageList from "./components/ImageList";
 import UploadForm from "./components/UploadForm";
 
 const App = () => {
   return (
-    <div>
+    <div style={{ maxWidth: 600, margin:"auto"}}>
       <ToastContainer/>
       <h2>사진첩</h2>
       <UploadForm/>
+      <ImageList/>
     </div>
     
   );
