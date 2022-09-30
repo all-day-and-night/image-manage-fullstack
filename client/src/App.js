0c0fa307aeda46a9b215afea16b16b381c615b57
index <jwug0821@gmail.com>
2022-09-30 13:01:54 +0900

    apply route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
-import ImageList from "./components/ImageList";
-import UploadForm from "./components/UploadForm";
+import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
+import RegisterPage from "./pages/RegisterPage";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Toolbar from "./components/Toolbar";
 
 const App = () => {
   return (
     <div style={{ maxWidth: 600, margin:"auto"}}>
       <ToastContainer/>
-      <h2>사진첩</h2>
-      <UploadForm/>
-      <ImageList/>
+      <Toolbar/>
+      <Routes>
+        <Route path="/" element={ <MainPage /> }/>
+        <Route path="/auth/register" element={ <RegisterPage />}/>
+        <Route path="/auth/login" element={ <LoginPage /> }/>
+      </Routes>
     </div>
-    
   );
 }
 
