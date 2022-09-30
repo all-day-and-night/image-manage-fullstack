0168e6139bbe23b20cc440b18398625ea6a3886d
index <jwug0821@gmail.com>
2022-09-30 17:22:04 +0900

    user Router

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -20,7 +20,7 @@ userRouter.post("/register", async (req, res) => {
         const session = user.sessions[0];
         res.json({ 
             successOrNot: "Y", 
-            sessionId: session,
+            sessionId: session._id,
             name: user.name
         });
     } catch(err){
@@ -56,6 +56,21 @@ userRouter.patch("/logout", async(req, res) => {
         );
         res.json({successOrNot:"Y"});
     } catch(err){
+        res.status(400).json({successOrNot:"N", errorMessage: err.message});
+    }
+});
+
+userRouter.get("/me", (req, res) => {
+    try{
+        if(!req.user) throw new Error("권한이 없습니다.");
+        res.json({
+            successOrNot: "Y",
+            sessionId: req.headers.sessionid,
+            name: req.user.name,
+            userId: req.user._id
+        });
+    } catch(err){
+        console.log(err);
         res.status(400).json({successOrNot:"N", errorMessage: err.message});
     }
 })
