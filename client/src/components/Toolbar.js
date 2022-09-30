61bf1cbda2c731cc0d6b3f578d1304b0bb298c24
index <jwug0821@gmail.com>
2022-09-30 17:20:47 +0900

    toolbar

--- client/src/components/Toolbar.js
+++ client/src/components/Toolbar.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Toolbar = () => {
+    const [me, setMe] = useContext(AuthContext);
+    
+    const logoutHandler = async () => {
+        try{
+            await axios.patch(
+                "/users/logout", 
+                {},
+                { headers: {sessionid: me.sessionId}}
+            );
+            setMe();
+            localStorage.removeItem("sessionId");
+            toast.success("로그아웃!");
+        } catch(err){
+            console.error(err);
+            toast.error(err.message);
+        }
+    }
     return (
         <div>
             <Link to="/"><span>홈</span></Link>
-            <Link to="/auth/login"><span style={{ float:"right"}}>로그인</span></Link>
-            <Link to="/auth/register"><span style={{float:"right", marginRight:15}}>회원가입</span></Link>
+            {me ? (
+                <span onClick={logoutHandler} style={{ float:"right"}}>로그아웃({me.name})</span>
+            )
+            : (<>
+            <Link to="/auth/login">
+                <span style={{ float: "right" }}>로그인</span>
+            </Link>
+            <Link to="/auth/register">
+                <span style={{ float: "right", marginRight: 15 }}>회원가입</span>
+            </Link></>)}
         </div>
     );
 }
