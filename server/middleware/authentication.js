76d83f37d3bc9cdbf868b4cb8464b81adf4f4608
index <jwug0821@gmail.com>
2022-09-30 17:21:11 +0900

    auth middleware

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -3,6 +3,7 @@ const User = require('../models/User');
 
 const authenticate = async (req, res, next) => {
     const { sessionid } = req.headers;
+    console.log({sessionid});
     if(!sessionid || !mongoose.isValidObjectId(sessionid)) return next();
     const user = await User.findOne({"sessions._id" : sessionid});
     if(!user) return next();
