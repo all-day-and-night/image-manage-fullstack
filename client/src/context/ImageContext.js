d8a0975781b8313f54885b5d97d66bdbf4906993
index <jwug0821@gmail.com>
2022-10-08 23:28:45 +0900

    me state hook

--- client/src/context/ImageContext.js
+++ client/src/context/ImageContext.js
@@ -1,11 +1,15 @@
 import axios from "axios";
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 import App from "../App";
+import { AuthContext } from "./AuthContext";
 
 export const ImageContext = createContext();
 
 export const ImageProvider = (prop) => {
     const [images, setImages] = useState([]);
+    const [myImages, setMyImages] = useState([]);
+    const [isPublic, setIsPublic] = useState(false);
+    const [me] = useContext(AuthContext);
     useEffect(() => {
         axios
             .get("/images")
@@ -16,8 +20,17 @@ export const ImageProvider = (prop) => {
             
     }, []);
 
+    useEffect(()=>{
+        if(me){
+            axios
+            .get("/users/me/images")
+            .then((result)=> setMyImages(result.data))
+            .catch(err => console.error(err));
+        }
+    }, [me]);
+
     return (
-        <ImageContext.Provider value={[images, setImages]}>
+        <ImageContext.Provider value={{ images, setImages, myImages,  setMyImages, isPublic, setIsPublic }}>
             {prop.children}
         </ImageContext.Provider>
     );
