1462eeee0ecc5adcf6bc854e25d38ceedb44d745
index <jwug0821@gmail.com>
2022-10-08 23:28:11 +0900

    public/private button

--- client/src/components/ImageList.js
+++ client/src/components/ImageList.js
@@ -1,13 +1,15 @@
 import React, { useContext, useEffect, useState }  from "react";
+import { AuthContext } from "../context/AuthContext";
 import { ImageContext } from "../context/ImageContext";
 
 const ImageList = () => {
-
-    const [images] = useContext(ImageContext);
+    const { images, myImages, isPublic, setIsPublic } = useContext(ImageContext);
+    const [me] = useContext(AuthContext);
     // eslint-disable-next-line jsx-a11y/alt-text
-    const imgList = images.map(image => 
+    const imgList = (me && isPublic ? myImages : images).map(image => 
     (
         <img 
+            alt=""
             key={image.key} 
             style={ {width:"100%"}} 
             src={`http://localhost:5001/uploads/${image.key}`}
@@ -16,7 +18,11 @@ const ImageList = () => {
     
     return (
         <div>
-            ImageList
+  
+            <h3 style={{ display: "inline-block", marginRight: 10 }}>
+                Image List
+                {isPublic ? "개인" : "공개"} 사진
+            </h3><button onClick={() => { setIsPublic(!isPublic); } }>{isPublic ? "공개" : "개인"} 사진보기</button>
             {imgList}
         </div>
     );
